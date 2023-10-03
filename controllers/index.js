a67330c83b2fd0d27a195ce04ae334f6bac1299d
Hazel <hazelchavez56@gmail.com>
2023-10-03 14:28:42 -0600

    Cambiando res.end

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
 const http = require("http");
 
+/* El código `const server = http.createServer((req, res) =>{...});` está creando un servidor HTTP
+usando el módulo `http` en Node.js. */
 const server = http.createServer((req, res) =>{
+/* El código `res.writeHead(200, {"Content-Type": "text/plain"});` está configurando el encabezado de
+respuesta HTTP con un código de estado de 200 (que significa "OK") y un tipo de contenido de " Texto
+sin formato". */
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World");
+    res.end("/screens/registrar_cliente.html");
 }); 
 
 const PORT = process.env.PORT || 3000;
